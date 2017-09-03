3b111682fea154bca12802d447e3be4a09704f95
Anastasia Lefa <swswlef@gmail.com>
2017-09-04 00:24:08 +0300

    Update TwitterStreamGlobe.js

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -163,7 +163,7 @@
 	 */ 
 	function render () {
 
-		earthMesh.rotation.y = earthMesh.rotation.y + 0.050;
+		earthMesh.rotation.y = earthMesh.rotation.y + 0.025;
 		
 	  renderer.autoClear = false;
 	  renderer.clear();
