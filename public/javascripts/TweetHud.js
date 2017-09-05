5ba66318f793e5828f490ee947713346fec42885
Anastasia Lefa <swswlef@gmail.com>
2017-09-05 14:56:04 +0300

    Update TweetHud.js

--- public/javascripts/TweetHud.js
+++ public/javascripts/TweetHud.js
@@ -92,12 +92,19 @@ app.controller('TweetHud', function($scope, $resource, $timeout, $rootScope, $ti
 		
 		var state = 'neutral';
 
-  	if (score < 0) {
+  	if (score < -1) {
   		state = 'negative';
   	}
-  	else if (score > 0) {
+  	else if (score > 1) {
   		state = 'positive';
   	}
+	else if (score < 0 & score < -1 ) {
+		state = 'negative1'
+	}
+        else if (score > 0 & score < 1 ) {
+		state = 'positive1'
+    
+    
 
 		return state;
 	}
